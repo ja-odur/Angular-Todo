8b1ddad74f0c4dbcac480a67c4ea653a399d9cd4
ja-odur <odurjoseph8@gmail.com>
2024-09-09 22:47:53 +0300

    Setup deleting tasks

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-import { Task } from '../../Task';
-import { TASKS } from '../../mock-tasks';
+import { Component } from "@angular/core";
+import { TaskService } from "../../services/task.service";
+import { Task } from "../../Task";
 
 @Component({
-  selector: 'app-tasks',
-  templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  selector: "app-tasks",
+  templateUrl: "./tasks.component.html",
+  styleUrl: "./tasks.component.css",
 })
 export class TasksComponent {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id)),
+      );
+  }
 }
